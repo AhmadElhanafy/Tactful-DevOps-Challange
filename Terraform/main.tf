22af73aac652bc10ee703c578e7bc10a40a22a78
Ahmad Taher Ahmad <ahmad.taher.elhanafy@gmail.com>
2025-11-20 00:30:49 +0200

    Extra terraform files

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "votingappacr"
+  name                = "tactfulvotingappacr"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
