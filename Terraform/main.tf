284bb2ee5796b351fd045bca7430b6db67c79374
Ahmad <Ahmad.Taher.Elhanafy@gmail.com>
2025-11-19 13:24:53 +0200

    added redis and postgres with networking

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
     }
   }
 
-  backend "local" {}   # we can switch to remote backend later if needed
+  backend "local" {}
 }
 
 provider "azurerm" {
@@ -22,6 +22,21 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "${var.project_name}-rg"
   location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "votingappacr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  sku                 = "Standard"
+  admin_enabled       = false
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
 }
 
 #
